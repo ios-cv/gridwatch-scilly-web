563ac5c22106d6468dea1697c3d02591cb6a21a0
George Goldberg <george@appletreetechnology.com>
2022-10-16 20:46:54 +0100

    Hook up the graph to real data from the API. (#8)
    Fixes #2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { DateTime } from 'luxon';
 
 import './App.css';
 import PowerTimeSeriesLineChart from './PowerTimeSeriesLineChart';
 
 function App() {
+  const [totalPower, setTotalPower] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/grid/flow')
+      .then((response) => response.json())
+      .then((data) => {
+        const d = data.map((i) => ({
+          power: i.power,
+          time: DateTime.fromISO(i.time).toSeconds(),
+        }));
+        setTotalPower(d);
+      });
+  }, []);
+
   return (
     <div className="App">
       <main>
-        <div style={{ height: '400px', width: '100%' }}>
-          <PowerTimeSeriesLineChart />
+        <div style={{
+          height: '400px',
+          width: '100%',
+        }}
+        >
+          <PowerTimeSeriesLineChart data={totalPower} />
         </div>
 
       </main>
--- src/PowerTimeSeriesLineChart.js
+++ src/PowerTimeSeriesLineChart.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { DateTime } from 'luxon';
 
 import {
   LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Tooltip,
 } from 'recharts';
 
-const data = [
-  { time: '20:55', power: 400 },
-  { time: '21:00', power: 300 },
-  { time: '21:05', power: 200 },
-  { time: '21:10', power: 350 },
-  { time: '21:15', power: 500 },
-];
+const timeFormatter = (date) => {
+  if (typeof date !== 'number') {
+    return date;
+  }
 
-function PowerTimeSeriesLineChart() {
+  if (date === 0) {
+    return date;
+  }
+
+  return DateTime.fromSeconds(date)
+    .toFormat('HH:mm');
+};
+
+const powerFormatter = (power) => `${power} MW`;
+
+function PowerTimeSeriesLineChart(props) {
+  const { data } = props;
+
   return (
     <ResponsiveContainer width="100%" height="100%">
       <LineChart
@@ -27,13 +39,22 @@ function PowerTimeSeriesLineChart() {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="time" />
-        <YAxis />
+        <XAxis dataKey="time" scale="time" tickFormatter={timeFormatter} interval={11} />
+        <YAxis tickFormatter={powerFormatter} />
         <Tooltip />
-        <Line type="monotone" dataKey="power" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="power" stroke="#8884d8" dot={false} />
       </LineChart>
     </ResponsiveContainer>
   );
 }
+
+PowerTimeSeriesLineChart.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      time: PropTypes.number,
+      power: PropTypes.number,
+    }),
+  ).isRequired,
+};
 
 export default PowerTimeSeriesLineChart;
